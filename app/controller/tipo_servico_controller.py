b52877472a9d20be7f94da951c513a654cc2e601
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2022-05-16 00:48:57 -0300

    feat: alow POST methods from tipo-servico

--- app/controller/tipo_servico_controller.py
+++ app/controller/tipo_servico_controller.py
@@ -1,16 +1,27 @@
+from flask import request
+
 from app import app
 from app.model.DAO.tipo_servico_dao import TipoServicoDAO
 
 
-@app.route("/tipo-servico")
+@app.route("/tipo-servico", methods=["GET", "POST"])
 def get_tipo_servico():
     dao = TipoServicoDAO()
-    results = list(ts.get_json() for ts in dao.select_all())
 
-    return {
-        'message': 'success',
-        'response': results
-    }
+    if request.method == 'GET':
+        results = list(ts.get_json() for ts in dao.select_all())
 
-print('a')
+        return {
+            'message': 'success',
+            'response': results
+        }
+
+    if request.method == 'POST':
+        j = request.get_json()
+        dao.insert((j['descricao'], j['valor']))
+
+        return {
+            'message': 'success'
+        }, 201
+
 
